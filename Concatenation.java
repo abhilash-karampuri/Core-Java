88c2e6f5131b5015b47a160474d681182c7d88eb
Abhilash Karampuri <abhilashkarampuri58@gmail.com>
2024-08-06 10:25:08 +0530

    Create Concatenation.java

---
+++ Concatenation.java
@@ -0,0 +1,20 @@
+public class Demo3 
+{
+	
+		
+	public static void main(String[] args) 
+	{
+			
+		System.out.println( "Ramesh"+5 );
+		
+		System.out.println( 4 + " Ramesh"  );
+				
+		System.out.println( "Ramesh " + " Kumar"  );
+		
+		System.out.println( 4 + 5  );    // + is Arithmetic
+				
+		
+	}	
+		
+		
+}
